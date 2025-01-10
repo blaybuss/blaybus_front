86507a2fd325eeb7dad5b5d83720102e88d8481f
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2025-01-10 23:20:04 +0900

    로그인

--- app/login.tsx
+++ app/login.tsx
@@ -1,16 +1,31 @@
 import React from "react";
-import { StyleSheet, View, Button, Text } from "react-native";
+import { StyleSheet, View, Text, TextInput } from "react-native";
+import Logo from "../assets/images/LOGO.svg";
 
 export default function LoginPage() {
-    return (
-        <View>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>로그인</Text>
+      <Logo width={50} height={100}/>
 
-        </View>
-    );
+      <Text>아이디</Text>
+      <TextInput/>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginVertical: 20,
+  },
+});
+
