5d4381712489b22530386c2005fc73879388e691
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2025-01-10 22:47:10 +0900

    로그인 구현 중

---
+++ app/login.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, View, Button, Text } from "react-native";
+
+export default function LoginPage() {
+    return (
+        <View>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        
+    }
+})
